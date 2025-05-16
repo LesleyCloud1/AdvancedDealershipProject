bddc960aebb67cbd811117e9350caf9957f820f9
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-05-16 13:23:08 -0500

    Created abstrcat class for contract.

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
-public class Contract {
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicle;
+
+    //constructor
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
+    }
+    //Abstract methods for subclasses to implement
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
+
+    //Getters
+    public String getDate() {
+        return date;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
 }
